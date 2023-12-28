f4c479e6816746535511c615392a36f55ac00dd7
mugeon <pos04167@kakao.com>
2023-12-28 22:07:27 +0900

    item1

---
+++ src/main/java/com/example/effectivejava/item1/Order.java
@@ -0,0 +1,29 @@
+package com.example.effectivejava.item1;
+
+public class Order {
+    private boolean prime;
+    private boolean urgent;
+    private Product product;
+
+
+    public Order() {
+
+    }
+
+
+
+    public static Order primeOrder(Product product) {
+        Order order = new Order();
+        order.urgent = true;
+        order.product = product;
+        return order;
+    }
+
+    public static Order urgentOrder(Product product) {
+        Order order = new Order();
+        order.prime = true;
+        order.product = product;
+        return order;
+    }
+
+}
